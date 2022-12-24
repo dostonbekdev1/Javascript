aee453ea7fe7e75c3f84a937b1560c0c5cbb7126
Dostonbek <dostonbekabdusattarov9@gmail.com>
2022-12-24 23:47:09 +0500

    Try ,   catch

--- 24-12/app.js
+++ 24-12/app.js
@@ -1,6 +1,6 @@
-// try{console.log(Hello)}
-// catch(err){
-// }
-// try{console.log("Hello")}
-// catch(err){
-// }
+try{console.log(Hello)}
+catch(err){
+}
+try{console.log("Hello")}
+catch(err){
+}
